491f9dd4139a674e316904ca9cfe5db4df95e73f
ananyadhananjaya <ananya.dhananjaya1998@gmail.com>
2021-09-10 23:46:50 +0530

    axios image get

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [image, setImage] = useState(false);
+  const [url, setUrl] = useState()
+  const handleClick = () =>{
+    axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+    .then((data)=>{
+      console.log(data.data)
+      setImage(true);
+      setUrl(data.data.url)
+    })
+    .catch((error)=>{
+      console.log(error)
+    })
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+      { !image && <button onClick={handleClick}>click</button>}
+      { image && <img src = {url} />}
     </div>
   );
 }
