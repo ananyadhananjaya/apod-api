354347646bdc442e10613cd5b50fef40289d8c40
ananyadhananjaya <ananya.dhananjaya1998@gmail.com>
2021-09-21 13:12:34 +0530

    hosted apod-api

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
-import { useEffect, useState } from 'react';
-import axios from 'axios';
-
-
-import './App.css';
 import Header from './Header/Header';
 import Content from './Content/Content';
 import Image from './ImageNasa/Image';
+
+
 
 function App() {
   return (
